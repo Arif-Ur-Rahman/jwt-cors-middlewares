724e95ab2040586a3d47226b8b1579ab991f4083
Arif Ur Rahman <arifurrahman.it.doc@gmail.com>
2025-09-10 12:04:54 +0600

    cookies updates

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -1,10 +1,14 @@
-export default function UserProfile({params}: any) {
+export default async function UserProfile({ params }: { params: Promise<{ id: string }> }) {
+  const { id } = await params;
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2 bg-black text-red-500">
-      <h1 className="text-green-500">Profile Page</h1>
+      <h1 className="text-green-500 mb-6">Profile Page</h1>
       <hr />
-      <p className="text-4xl text-red-500">This is a protected profile page
-        <span className="p-2 mx-2 mr-2 rounded bg-orange-600 text-black">{params.id}</span></p>
+      <p className="text-4xl text-red-500">
+        This is profile page
+        <span className="p-2 mx-2 mr-2 rounded bg-orange-600 text-black">{id}</span>
+      </p>
     </div>
   );
 }
